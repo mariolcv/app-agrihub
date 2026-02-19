12904497b3b37b1d039caeb4d13f4e48a7f81040
Mario Lacueva <mariolacueva01@gmail.com>
2026-02-19 13:38:41 +0800

    vercel config

--- .devcontainer/install_flutter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/flutter/flutter.git -b stable --depth 1 /usr/local/flutter
-
-echo 'export PATH="/usr/local/flutter/bin:$PATH"' >> ~/.bashrc
-export PATH="/usr/local/flutter/bin:$PATH"
-
-flutter config --enable-web
-flutter doctor
